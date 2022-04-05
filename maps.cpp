046d64bbfa561bcf698a42bc60ac1cfa6321aa80
tanmayhinge <hingetanmay@gmail.com>
2022-04-05 20:56:56 +0530

    map

---
+++ maps.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    map<char,int> m = { //basic map declaration
+        {'a', 2},
+        {'b', 3},
+        {'c', 0}
+    };
+    m['d'] = 3; // adding new element to map
+    cout<<m['d']<<endl;
+    
+    pair<char,int> p = {'e', 4};
+    
+    m.insert(p); // insert a new pair to the map
+    
+    cout<<m['e']<<endl;
+    
+    m.clear(); // empties the map
+    
+    cout<<m.empty()<<endl; // returns 1 if empty and 0 if not empty
+    
+}
