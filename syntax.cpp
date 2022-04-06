810bc3cd4140f2aee5152c8b9710223f52f148af
tanmayhinge <hingetanmay@gmail.com>
2022-04-06 23:34:44 +0530

    syntax++

--- syntax.cpp
+++ syntax.cpp
@@ -1,9 +1,18 @@
 #include<bits/stdc++.h> // header files, here this particular header file imports all the things you need for competitive programming
 
-using namespace std; // specifies that we are working in the "standard" namespace
+using namespace std; // specifies that we are working in the "standard" namespace, so basically you dont need to write std:: before all the non native C code all the time i.e. cout, cin, endl etc...
 
 int main(){ // entry point for the program
-    int a = 1;
-    int b;
-    string a = "hello";
+    int a = 1; // integer declaration
+    int b; // integer declaration
+    b = a; // b takes the value of a
+    string s1("hello"); // C++ style string declaration
+    
+    cout<<a; // output a variable
+    cout<<endl; // output a new line
+    cout<<"Hello World" // output a string
+    
+    cout<<"Hey"<<s1 <<" "<<b <<endl; // multiple outputs
+    
+    
 }
