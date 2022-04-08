1842b349f8c1fd4ce843dadc70d6ba5a7720d313
tanmayhinge <hingetanmay@gmail.com>
2022-04-08 13:08:21 +0530

    minor loop change

--- TOTCRT.cpp
+++ TOTCRT.cpp
@@ -27,7 +27,9 @@ int main(){
           v.push_back(i.second);  
         } 
         sort(v.begin(), v.end());
-        for (int i = 0; i < v.size(); i++) cout << v[i] << " ";
+        for (int i = 0; i < v.size(); i++){
+            cout << v[i] << " ";
+        }
         cout << endl;
         
         
